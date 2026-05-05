1e58acb3387b53194f6ae17140ec7eb9260c3428
Arve Nilsen <116888230+ArveNilsen@users.noreply.github.com>
2026-05-05 22:27:52 +0200

    Put together a very simple example

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include <arpa/inet.h>
+
+#include "RawSocket.hpp"
+#include "SocketFactory.hpp"
+#include "BoundSocket.hpp"
+#include "SysPolicy.hpp"
 
 int main()
 {
-    std::cout << "Hello, Sockets!\n";
+    SocketFactory<SysPosix> factory;
+    RawSocket rs = factory.create_tcp();
+
+    sockaddr_in addr{};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(8080);
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    // TODO: Needs a cleaner api, hide the details
+    BoundSocket bound = factory.bind_address(std::move(rs), 
+            reinterpret_cast<sockaddr*>(&addr), sizeof(addr));
+
+    int fd = bound.native_handle();
+
+    std::cout << "BoundSocket fd: " << fd << "\n";
     return 0;
 }
